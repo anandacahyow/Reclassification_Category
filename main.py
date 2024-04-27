c54395f9b5b40042fe80aba8c8d1ac446e1d047c
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-04-27 22:12:33 +0700

    wide margin

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.beta_set_page_config(layout="wide")
+
 # Step 1: Read the Excel file and preprocess the data
 @st.cache
 def load_data(file_path):
