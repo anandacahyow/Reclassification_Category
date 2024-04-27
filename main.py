30e35db2b0731a9878bc088ab9cecea3bb5d9cb4
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-04-27 22:25:48 +0700

    height 400

--- main.py
+++ main.py
@@ -79,7 +79,8 @@ def create_timeline(df, start_date, end_date, start_time, end_time, selected_cat
     fig.update_layout(title="Duration of Original Categories",
                       xaxis_title="Datetime",
                       yaxis_title=y_axis,
-                      width = 1200)
+                      width = 1200,
+                      height = 400)
     st.plotly_chart(fig)
 
 # Step 2: Create a Streamlit app
