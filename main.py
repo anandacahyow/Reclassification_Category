37aac6be0b7402437a3683362b89a9cab65556f4
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-04-27 23:33:05 +0700

    no line

--- main.py
+++ main.py
@@ -98,9 +98,9 @@ def create_pareto(df, category_column, value_column):
     # Plot Pareto diagram
     fig = px.bar(df_sorted, x=category_column, y=value_column, title=f"Pareto Diagram - {category_column}",
                  labels={category_column: "Categories", value_column: "Duration (s)"})
-    fig.add_scatter(x=df_sorted[category_column], y=df_sorted["cumulative_percentage"], mode="lines", line=dict(color="red"),
+    #fig.add_scatter(x=df_sorted[category_column], y=df_sorted["cumulative_percentage"], mode="lines", line=dict(color="red"),
                     name="Cumulative Percentage",secondary_y=True)
-    fig.update_layout(yaxis2=dict(title="Cumulative Percentage"))
+    #fig.update_layout(yaxis2=dict(title="Cumulative Percentage"))
     st.plotly_chart(fig)
 
 # Step 2: Create a Streamlit app
