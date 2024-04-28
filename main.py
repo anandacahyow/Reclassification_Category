cd9e9d4e976cb4cc2d5c055622b7837adf9ee4b5
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-04-28 14:34:50 +0700

    adj timeline3

--- main.py
+++ main.py
@@ -32,9 +32,13 @@ def create_timeline(df, start_date, end_date, start_time, end_time, selected_cat
         "Planned Stoppages": "yellow"
     }
 
+    # Convert start and end times to timedelta objects
+    start_timedelta = pd.to_timedelta(start_time)
+    end_timedelta = pd.to_timedelta(end_time)
+
     # Combine start date with start time and end date with end time
-    start_datetime = pd.to_datetime(start_date + " " + start_time)
-    end_datetime = pd.to_datetime(end_date + " " + end_time)
+    start_datetime = pd.to_datetime(start_date) + start_timedelta
+    end_datetime = pd.to_datetime(end_date) + end_timedelta
 
     # Filter data based on selected categories and date range
     filtered_df = df[(df['Original Category'].isin(selected_categories)) &
@@ -88,6 +92,7 @@ def create_timeline(df, start_date, end_date, start_time, end_time, selected_cat
                       width=1200,
                       height=400)
     st.plotly_chart(fig)
+
 
 def create_pareto(df, category_column, value_column,duration_type):
     # Group data by category and sum the duration
