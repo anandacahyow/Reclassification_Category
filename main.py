82bc86b1ca3acd79c997df1ce994f0333c36cdb9
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-04-28 16:10:20 +0700

    waterfall

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def create_waterfall(df, category_column1, category_column2, value_column, durat
     fig = go.Figure(go.Waterfall(
         x=categories,
         y=values,
-        measure=["relative" if val != 0 else "total" for val in values],  # Different measure for each bar
+        measure=["relative" if val != 1 else "total" for val in values],  # Different measure for each bar
         base=-10,  # Set the base to 100
         increasing=dict(marker=dict(color="green")),  # Set color for increasing values
         decreasing=dict(marker=dict(color="red")),  # Set color for decreasing values
