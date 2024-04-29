e0cd606752cced6a9ba84e4fc4f176a9e5cfd155
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-04-29 08:13:34 +0700

    URL Foter

--- main.py
+++ main.py
@@ -294,8 +294,8 @@ def main():
         create_waterfall(filtered_df,"Original Category","Reclassified Category", "Duration", duration_type)
         
         st.sidebar.image("Nestle_Signature.png")
-        st.sidebar.write("""<p style='font-size: 14px;'>This Web-App is designed to facilitate DOR member of PT Nestlé Indonesia - Panjang Factory in identifying DMO Performance Category reclassification and track compliance based on <b>St-21.908-03 - Manufacturing Resources Performance Measurement Definition and Calculations
-<b></p>""", unsafe_allow_html=True)
+        url = "https://nestle.sharepoint.com/:b:/t/NMTTechnical2023/EZ2DQYyVfblDhGV11hbULU0BAPm34HHC5ZHCUERmFu3tnQ?e=frKMsh"
+        st.sidebar.write("""<p style='font-size: 14px;'>This Web-App is designed to facilitate DOR member of PT Nestlé Indonesia - Panjang Factory in identifying DMO Performance Category reclassification and track compliance based on <b><a href=url style="color:blue;">St-21.908-03 - Manufacturing Resources Performance Measurement Definition and Calculations</a></b></p>""", unsafe_allow_html=True)
         st.sidebar.write("""<p style='font-size: 13px;'>For any inquiries, error handling, or assistance, please feel free to reach us through Email: <br>
 <a href="mailto:Ananda.Cahyo@id.nestle.com">Ananda.Cahyo@id.nestle.com <br></p>""", unsafe_allow_html=True)
 
