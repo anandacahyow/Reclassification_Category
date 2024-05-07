709fcde8b1166684414103f1645621a3778155f9
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-05-07 12:12:13 +0700

    added new pareto each

--- main.py
+++ main.py
@@ -302,7 +302,10 @@ def main():
         
         create_waterfall(filtered_df,"Original Category","Reclassified Category", "Duration", duration_type)
 
-        create_pareto(filtered_df, "Reclassified Reason", "Duration", duration_type)
+        available_category = df['Reclassified Category'].unique()
+        for categories in available_category:
+            data_cat = filtered_df[filtered_df['Reclassified Category'] == categories]
+            create_pareto(data_cat, "Reclassified Reason", "Duration", duration_type)
         
     st.sidebar.image("Nestle_Signature.png")
     st.sidebar.write("""<p style='font-size: 14px;'>This Web-App is designed to facilitate DOR member of PT Nestlé Indonesia - Panjang Factory in identifying DMO Performance Category reclassification and track compliance based on <b><a href="https://nestle.sharepoint.com/:b:/t/NMTTechnical2023/EZ2DQYyVfblDhGV11hbULU0BAPm34HHC5ZHCUERmFu3tnQ?e=IdQUp4" style="color:blue;">St-21.908-03 - Manufacturing Resources Performance Measurement Definition and Calculations</a></b></p>""", unsafe_allow_html=True)
