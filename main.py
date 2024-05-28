e4fe7d1bd34b0415b4d2958e0fec2fb2b1397520
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-05-28 08:44:44 +0700

    cascaded

--- main.py
+++ main.py
@@ -333,17 +333,19 @@ def main():
         # Filter the dataframe based on the selected column and value
         filtered_df = df[df[filter_column] == filter_value]
         
-        # Loop through available categories and create Pareto charts and tables for each
-        available_category = filtered_df[default_cat].unique()
-        for category in available_category:
-            data_cat = filtered_df[filtered_df[default_cat] == category]
-            col1, col2 = st.columns(2)
-            with col1:
-                create_pareto(data_cat, selected_header, "Duration", duration_type, default_cat)
-            with col2:
-                st.write(data_cat)
-
-
+        # Verify if the selected header exists in the filtered dataframe
+        if selected_header not in filtered_df.columns:
+            st.error(f"Column '{selected_header}' not found in the filtered dataframe.")
+        else:
+            # Loop through available categories and create Pareto charts and tables for each
+            available_category = filtered_df[default_cat].unique()
+            for category in available_category:
+                data_cat = filtered_df[filtered_df[default_cat] == category]
+                col1, col2 = st.columns(2)
+                with col1:
+                    create_pareto(data_cat, selected_header, "Duration", duration_type, default_cat)
+                with col2:
+                    st.write(data_cat)
         
     st.sidebar.image("Nestle_Signature.png")
     st.sidebar.write("""<p style='font-size: 14px;'>This Web-App is designed to facilitate DOR member of PT Nestlé Indonesia - Panjang Factory in identifying DMO Performance Category reclassification and track compliance based on <b><a href="https://nestle.sharepoint.com/:b:/t/NMTTechnical2023/EZ2DQYyVfblDhGV11hbULU0BAPm34HHC5ZHCUERmFu3tnQ?e=IdQUp4" style="color:blue;">St-21.908-03 - Manufacturing Resources Performance Measurement Definition and Calculations</a></b></p>""", unsafe_allow_html=True)
