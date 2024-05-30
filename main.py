feb3461f3279c4361848cce3d79663ff07cd5bae
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-05-30 14:41:49 +0700

    selected header

--- main.py
+++ main.py
@@ -423,7 +423,8 @@ def main():
             data_cat = filtered_df[filtered_df[selected_header_filter] == equipment]
             col1, col2 = st.columns(2)
             with col1:
-                create_pareto_with_colors(data_cat, "Reclassified Reason", "Duration", duration_type, equipment, color_column='Reclassified Category')
+                #create_pareto_with_colors(data_cat, "Reclassified Reason", "Duration", duration_type, equipment, color_column='Reclassified Category')
+                create_pareto_with_colors(data_cat, selected_header_filter, "Duration", duration_type, equipment, color_column='Reclassified Category')
             with col2:
                 st.write(data_cat, height=450, width=150)
 
