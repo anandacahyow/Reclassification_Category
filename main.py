1ef18c04841133f8163f2574316581d082d13dc2
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-06-07 08:40:52 +0700

    Checkpoint

--- main.py
+++ main.py
@@ -424,7 +424,6 @@ def main():
             col1, col2 = st.columns(2)
             with col1:
                 #create_pareto_with_colors(data_cat, "Reclassified Reason", "Duration", duration_type, equipment, color_column='Reclassified Category')
-                st.write(selected_header2)
                 create_pareto_with_colors(data_cat, selected_header2, "Duration", duration_type, equipment, color_column='Reclassified Category')
             with col2:
                 st.write(data_cat, height=450, width=150)
